d3b85e42624544c02911eb57ce896ec571e713c1
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-09-21 11:51:20 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+var kata = "Semangat ...."
+
+var hasil = kata.repeat(5)
+console.log (hasil)
+
+console.log()
+
+var kata1 = "*"
+var hasil2 = kata1.repeat(5)
+var hasil3 = kata1.repeat(10)
+var hasil4 = kata1.repeat(100)
+var hasil5 = kata1.repeat(50)
+var hasil6 = kata1.repeat(20)
+var hasil7 = kata1.repeat(15)
+console.log (hasil2)
+console.log (hasil3)
+console.log (hasil4)
+console.log (hasil5)
+console.log (hasil6)
+console.log (hasil7)
